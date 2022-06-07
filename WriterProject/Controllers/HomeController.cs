9d0f21a13707486adfc60fcddbe82f14464f8375
ertugrulocakee <ertugrulocakee@gmail.com>
2022-06-07 17:17:43 +0300

    Giriş Sayfası Eklendi.

--- WriterProject/Controllers/HomeController.cs
+++ WriterProject/Controllers/HomeController.cs
@@ -25,5 +25,16 @@ namespace WriterProject.Controllers
             ViewBag.Message = "Your contact page.";
             return View();
         }
+
+
+        [AllowAnonymous]
+        public ActionResult HomePage()
+        {
+
+            return View();
+
+        }
+
+
     }
 }
