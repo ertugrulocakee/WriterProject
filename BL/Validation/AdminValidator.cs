050e76092de3b04066e8bb0e7c2c166fc1698233
ertugrulocakee <ertugrulocakee@gmail.com>
2022-06-10 12:13:04 +0300

    Admin Paneli Düzenleme

--- BL/Validation/AdminValidator.cs
+++ BL/Validation/AdminValidator.cs
@@ -18,8 +18,8 @@ namespace BL.Validation
             RuleFor(x => x.userName).MaximumLength(40).WithMessage("Kullanıcı adı maksimum 40 karakterden oluşmalıdır!");
             RuleFor(x => x.password).NotEmpty().WithMessage("Şifre boş olamaz!");
             RuleFor(x => x.password).MaximumLength(20).WithMessage("Şifre maksimum 20 karakterden oluşmalıdır!");
-
-
+            RuleFor(x => x.email).NotEmpty().WithMessage("E-posta boş olamaz!");
+            RuleFor(x => x.email).MaximumLength(30).WithMessage("E-posta adresi maksimum 30 karakterden oluşmalıdır!");
 
         }
 
---
+++ WriterProject/Controllers/AdminController.cs
@@ -0,0 +1,166 @@
+﻿using BL.Concrete;
+using BL.Validation;
+using DAL.EF;
+using Entities.Concrete;
+using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WriterProject.Controllers
+{
+
+    [Authorize(Roles = "1")]
+    public class AdminController : Controller
+    {
+        // GET: Admin
+
+        AdminManager adminManager = new AdminManager(new EFAdminDAL());
+        WriterManager writerManager = new WriterManager(new EFWriterDAL());
+
+        [HttpGet]
+        public ActionResult AddAdmin()
+        {           
+            return View();
+        }
+
+        [HttpPost]  
+        public ActionResult AddAdmin(Admin admin)
+        {
+
+
+            AdminValidator adminValidator = new AdminValidator();
+            ValidationResult result = adminValidator.Validate(admin);
+
+
+            if (result.IsValid)
+            {
+
+                var admins = adminManager.TGetList().Where(m=>m.email == admin.email || m.userName == admin.userName).ToList();
+
+                if (admins.Any())
+                {
+
+                    ViewBag.Message = "E-posta adresiniz veya kullanıcı adınız bir adminle ortaktır! Lutfen farkli degerler deneyin!";
+
+                    return View();
+
+                }
+
+                var writers = writerManager.TGetList().Where(m => m.WriterMail == admin.email).ToList();
+
+                if (writers.Any())
+                {
+
+                    ViewBag.Message = "E-posta adresiniz  bir yazarla ortaktır! Lutfen farkli degerler deneyin!";
+
+                    return View();
+
+                }
+
+
+                admin.role = "1";
+
+                adminManager.TAdd(admin);
+
+                ViewBag.Message = "Yeni admin basariyla eklendi/tanimlandi!";
+
+                return View();  
+
+            }
+            else
+            {
+
+                foreach (var item in result.Errors)
+                {
+
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+
+                }
+
+            }
+
+            return View();
+
+        }
+
+
+
+        [HttpGet]
+        public ActionResult EditAdmin()
+        {
+
+            var admin = adminManager.GetAdmin(Session["AdminEmail"].ToString());
+
+            return View(admin);
+            
+        }
+
+        [HttpPost]
+        public ActionResult EditAdmin(Admin admin)
+        {
+
+            AdminValidator adminValidator = new AdminValidator();
+            ValidationResult result = adminValidator.Validate(admin);
+
+
+            if (result.IsValid)
+            {
+
+                var admins = adminManager.TGetList().Where(m => (m.email == admin.email || m.userName == admin.userName) && m.id != admin.id).ToList();
+
+                if (admins.Any())
+                {
+
+                    ViewBag.Message = "E-posta adresiniz veya kullanıcı adınız bir adminle ortaktır! Lutfen farkli degerler deneyin!";
+
+                    return View();
+
+                }
+
+                var writers = writerManager.TGetList().Where(m => m.WriterMail == admin.email).ToList();
+
+                if (writers.Any())
+                {
+
+                    ViewBag.Message = "E-posta adresiniz  bir yazarla ortaktır! Lutfen farkli degerler deneyin!";
+
+                    return View();
+
+                }
+
+                var value = adminManager.TGetByID(admin.id);
+
+                value.email = admin.email;
+                value.password = admin.password;
+                value.userName = admin.userName;
+
+                adminManager.TUpdate(value);
+
+                ViewBag.Message = "Admin basariyla guncellendi!";
+
+                return View();
+
+            }
+            else
+            {
+
+                foreach (var item in result.Errors)
+                {
+
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+
+                }
+
+            }
+
+            return View();
+
+
+        }
+
+
+    }
+}
